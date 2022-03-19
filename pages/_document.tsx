075a1a17080fd1232e9d3d1cba8a6e01de9459da
Clupai8o0 <clupaio4@gmail.com>
2022-03-19 11:59:48 +0300

    init proj

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, {
+	Html,
+	Head, //* for something that shows up in all pages
+	Main,
+	NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"
+						rel="stylesheet"
+					/>
+				</Head>
+
+				<body>
+					<Main></Main>
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
