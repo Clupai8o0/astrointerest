38ee00d80c01e9da4a2d1a5ee64ee3c7c538fb10
Clupai8o0 <clupaio4@gmail.com>
2022-03-21 10:37:21 +0300

    join today homepage section

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,10 +12,9 @@ export default function Home() {
 			<Head />
 			<NavBar />
 
-			{/* TODO: Each section is 100vw and 100vh min */}
 			<main className="w-screen flex flex-col items-center overflow-x-hidden">
 				{/* Hero */}
-				<section className="w-screen max-w-7xl h-screen pt-24 flex flex-col lg:items-center lg:flex-row-reverse lg:px-6">
+				<section className="section-container flex flex-col lg:items-center lg:flex-row-reverse lg:px-6">
 					<section className="relative w-full h-80 lg:w-1/2 md:h-[500px] lg:max-h-[578px] lg:h-[578px]">
 						<Image
 							src="/assets/hero-img.jpg"
@@ -39,7 +38,7 @@ export default function Home() {
 				</section>
 
 				{/* Save & Share */}
-				<section className="w-screen max-w-7xl h-screen pt-24 flex flex-col items-center lg:flex-row lg:px-6">
+				<section className="section-container flex flex-col items-center lg:flex-row lg:px-6">
 					<section className="relative w-[80%] h-64 lg:w-1/2 md:h-[300px] lg:max-h-[400px] lg:h-[350px]">
 						<Image
 							src="/assets/home/illustration-save.svg"
@@ -48,6 +47,7 @@ export default function Home() {
 							objectFit="contain"
 						/>
 					</section>
+
 					<section className="text-center mt-12 lg:text-left">
 						<h1 className="max-w-xs mb-4 md:max-w-lg md:mb-8">Save & Share</h1>
 						<p className="max-w-xs md:max-w-lg">
@@ -57,26 +57,18 @@ export default function Home() {
 					</section>
 				</section>
 
-				{/* Detailed info */}
-				{/* <section>
-					<div>
-						<video autoPlay loop style={{ width: "500px", height: "500px" }}>
-							<source src="//video-stars.mp4" />
-						</video>
-					</div>
-
-					<h1>Get detailed info about what you&apos;re looking at</h1>
-				</section> */}
-
 				{/* Join */}
-				{/* <section>
-					<div>
+				<section className="section-container flex items-center justify-center">
+					<div className="flex flex-col items-center">
 						<h1>Join Today</h1>
+
 						<Link href="/auth/register">
-							<a>Sign Up</a>
+							<a className="join-btn mt-8 heartbeat" title="Sign up">
+								<span>Sign Up</span>
+							</a>
 						</Link>
 					</div>
-				</section> */}
+				</section>
 			</main>
 
 			{/* <Footer /> */}
