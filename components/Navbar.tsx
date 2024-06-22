a14495f8c8989d0ef5cd6491d5e152ca0a806f0d
Clupai8o0 <clupaio4@gmail.com>
2024-06-22 11:53:52 +0300

    nearly forgot the logo text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ const Navbar = () => {
 	return (
 		<header className="w-full flex justify-center px-6 relative z-10">
 			<nav className="max-w-7xl w-full py-4 md:py-8 flex items-center justify-between">
-				<div>
+				<div className="flex items-center gap-2">
 					<img src="/logo.svg" alt="astrointerest logo of a telescope" />{" "}
-					<span className="hidden md:blocks">Astrointerest</span>
+					<span className="hidden md:block text-white text-lg font-semibold">Astrointerest</span>
 				</div>
 				<Button>Sign In</Button>
 			</nav>
