5c8a50c3b58e14d45fb99270fd2bd1ebda5fe364
Clupai8o0 <clupaio4@gmail.com>
2024-06-22 11:25:21 +0300

    navbar and basic implementation blocks done

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,22 @@
-import SignInButton from "@/components/SignInButton";
-import Image from "next/image";
-import Logo from "@/components/Logo";
+import Navbar from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <main className="flex h-screen flex-col bg-neutral-900 bg-cover bg-right sm:bg-[url('/pcBGHome.png')]">
-      <nav className= "flex justify-between bg-transparent">
-        <Logo/>
-        <div className="mx-10 my-5">
-          <SignInButton />
-        </div>
-        
-      </nav>
-      <div className="w-screen h-full bg-cover bg-right sm:bg-none bg-[url('/mobileBGHome.png')]">
-        <div className=" text-center my-28 sm:text-left sm:ml-24 sm:my-48">
-          <h2 className="text-4xl sm:text-7xl font-Raleway">Explore the</h2>
-          <h2 className="text-4xl font-extrabold italic sm:text-8xl font-Raleway">Cosmos</h2>
-          <p className="my-4 font-Raleway text-xs font-light mx-10 sm:mx-0 sm:text-lg sm:w-2/5 sm:text-start">Embark on a journey through the universe with our 
-            curated collection of high-quality space images. 
-            Save your favorite celestial snapshots, organize them into 
-            boards, and share the wonders of the cosmos with
-             others. Join our community of space enthusiasts and 
-             start exploring today!</p>
-             <SignInButton/>
-        </div>
-        
-      </div>
-    </main>
-  );
+	return (
+		<>
+			<Navbar />
+			<main className="relative">
+				<div>
+					<h1></h1>
+					<p></p>
+					<Button>Sign In</Button>
+				</div>
+
+				<img
+					src="/mobile-hero-img.png"
+					alt="astronomy pins aligned in mobile view"
+				/>
+			</main>
+		</>
+	);
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+	return (
+		<header className="w-full flex justify-center px-6">
+			<nav className="max-w-7xl w-full py-4 flex items-center justify-between">
+				<div>
+					<img src="/logo.svg" alt="astrointerest logo of a telescope" />{" "}
+					<span className="hidden md:blocks">Astrointerest</span>
+				</div>
+				<Button>Sign In</Button>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
